5437b64f42a98880e24fb8cf6f3644c1247a497d
matanini <matanini@gmail.com>
2023-03-08 15:44:36 +0200

    Update TSQueue.cpp
    TSQueue implemented

--- bioeye-assignment/TSQueue.cpp
+++ bioeye-assignment/TSQueue.cpp
@@ -1,21 +1,29 @@
 #include "TSQueue.h"
 
 template <class T>
-TSQueue<T>::TSQueue()
-{
-	
-}
+TSQueue<T>::TSQueue() : queue() {}
 
 template <class T>
 T TSQueue<T>::pop()
 {
-	
+	std::unique_lock<std::mutex> lock(m);
+	//while (queue.empty())
+	cv.wait(lock, [&]
+		{
+			return !queue.empty();
+		});
+	T data = queue.front();
+	queue.pop();
+	return data;
 }
 
 template <class T>
 void TSQueue<T>::push(T data)
 {
-	
+	std::unique_lock<std::mutex> lock(m);
+	queue.push(data);
+	lock.unlock();
+	cv.notify_all();
 }
 
 
